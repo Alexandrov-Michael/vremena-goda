b6d5ca8965d4848ef6120f41486725cd9e8d2906
Alexandrov-Michael <mikle.alex@gmail.com>
2012-10-08 08:12:22 +0400

    style gallery

--- project/proj/setting_dev.py
+++ project/proj/setting_dev.py
@@ -12,7 +12,7 @@ DATABASES = {
         'NAME': 'vremena',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '0',                  # Not used with sqlite3.
-        'HOST': '192.168.1.6',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
